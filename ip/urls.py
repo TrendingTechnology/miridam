c26f8902c97b048e5120ca9dc5c583e0523b8686
ssousuke <wesleysousukee@gmail.com>
2021-08-19 01:04:04 -0300

    refactor

--- ip/urls.py
+++ ip/urls.py
@@ -3,7 +3,7 @@ from ip import views
 
 app_name = 'ip'
 urlpatterns = [
-    path('', views.list_ipv4, name='list_ipv4'),
-    path('cadastrar_ipv4/', views.register_ipv4, name='register_ipv4'),
+    path('', views.IpListView.as_view(), name='list'),
+    path('cadastrar/', views.register_ipv4, name='register'),
 
 ]
--- ip/views.py
+++ ip/views.py
@@ -1,22 +1,37 @@
 from django.shortcuts import render, redirect
 from ip.forms import Ipv4Form
 from ip.models import IP
+from django.views import generic
 
 
-def list_ipv4(request):
+class IpListView(generic.ListView):
+    model = IP
     template_name = 'index.html'
-    list_all_ipv4 = IP.objects.filter(activated=True)
-    context = {'list_all_ipv4': list_all_ipv4}
-    return render(request, template_name, context)
+
+    # Definindo um contexto
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # context recebe um dicionário
+        context = super().get_context_data(**kwargs)
+        # context 'list_all_ip' recebe uma lista de IP's filtrados pelo statuc de activated
+        context['list_all_ip'] = IP.objects.filter(activated=True)
+        # Retorna o context
+        return context
 
 
 def register_ipv4(request):
+    # Se request do usuário for igual a POST
     if request.method == "POST":
+        # A variavel form vai receber os dados do request.POST
         form = Ipv4Form(request.POST)
+        # Se os dados recebeido pelo form for valido
         if form.is_valid():
-            ipv4 = form.save(commit=False)
-            ipv4.save()
-            return redirect('ip:list_ipv4')
+            # A Variavel ip vai receber e salvar o form mas não fará o commit para o banco de dados
+            ip = form.save(commit=False)
+            # Salvando ip (form) no banco de dados
+            ip.save()
+            # Redireciona para a pagina inicial
+            return redirect('ip:list')
     else:
+        # Caso o metodo não seja um POST válido ele vai retornar um form vazio
         form = Ipv4Form()
-    return render(request, 'register_ipv4.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
