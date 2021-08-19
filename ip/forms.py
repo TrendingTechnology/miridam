c68f59be2203fb3956b1e1b7dfef706e4d0a3ac4
ssousuke <wesleysousukee@gmail.com>
2021-08-19 02:03:15 -0300

    renomeia o nome da classe

--- ip/forms.py
+++ ip/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from ip.models import IP
 
 
-class Ipv4Form(ModelForm):
+class IpForm(ModelForm):
     class Meta:
         model = IP
         fields = ['ips_address', 'department', 'activated']
