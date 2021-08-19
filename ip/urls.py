79804671af933cdc7af82462788e0aa039646139
ssousuke <wesleysousukee@gmail.com>
2021-08-19 02:01:55 -0300

    adiciona urls de listar ip's desativados e deletar

--- ip/urls.py
+++ ip/urls.py
@@ -4,6 +4,8 @@ from ip import views
 app_name = 'ip'
 urlpatterns = [
     path('', views.IpListView.as_view(), name='list'),
+    path('ip_desativados/', views.disabled_IP_list, name='disable-list'),
     path('cadastrar/', views.register_ipv4, name='register'),
+    path('deletar/<int:pk>', views.IpDeleteView.as_view(), name='delete'),
 
 ]
