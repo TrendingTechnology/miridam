eb1ac814cbcfcce64b489686b84d71bbbdfa26c9
ssousuke <wesleysousukee@gmail.com>
2021-08-21 18:10:00 -0300

    form de ElectronicEquipment

--- ip/forms.py
+++ ip/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from ip.models import IP
+from ip.models import IP, ElectronicEquipment
 
 
 class IpForm(ModelForm):
     class Meta:
         model = IP
         fields = ['ips_address', 'activated']
+
+
+class ElectronicEquipmentForm(ModelForm):
+    class Meta:
+        model = ElectronicEquipment
+        fields = '__all__'
