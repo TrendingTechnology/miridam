b36f8cb55045cc9935ea9c6d4a5731ddc9e1ea40
ssousuke <wesleysousukee@gmail.com>
2021-08-21 17:10:49 -0300

    refactor - adiciona url de departamento

--- drogon/urls.py
+++ drogon/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('ip.urls', namespace='ip')),
+    path('departamento/', include('department.urls', namespace='department')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
