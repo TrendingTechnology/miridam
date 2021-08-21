801038eaf8587cd2ad1af320f1f7de37f7fde0b9
ssousuke <wesleysousukee@gmail.com>
2021-08-21 17:05:21 -0300

    adiciona os apps

--- drogon/settings.py
+++ drogon/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ip',
+    'ip.apps.IpConfig',
+    'department.apps.DepartmentConfig',
 ]
 
 MIDDLEWARE = [
