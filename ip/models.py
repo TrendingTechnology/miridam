bd9b27e7f136575c98980aa7d4eedd8a5d6a5733
ssousuke <wesleysousukee@gmail.com>
2021-08-21 17:06:48 -0300

    refactor e adiciona classe de equipamentos

--- ip/models.py
+++ ip/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+from department.models import Department
+
+EQUIPMENT_TYPE_CHOICES = (
+    ('TV', 'TV'),
+    ('DESKTOP', 'DESKTOP'),
+    ('NOTEBOOK', 'NOTEBOOK'),
+    ('SOM', 'SOM'),
+    ('ROTEADOR', 'ROTEADOR'),
+    ('CÀMERA', 'CÂMERA'),
+    ('OUTROS', 'OUTROS'),
+)
+
 
 class Base(models.Model):
     register_in = models.DateTimeField(auto_now_add=True)
@@ -11,10 +23,7 @@ class Base(models.Model):
 
 
 class IP(Base):
-    ips_address = models.GenericIPAddressField(unique=True, protocol='IPv4', verbose_name='Endereço IPv4:',
-                                               help_text='Exemplo: <a>192.168.0.12</a>.')
-    department = models.CharField(max_length=80, verbose_name='Departamento:',
-                                  help_text='Departamento: <a>Administração</a>')
+    ips_address = models.GenericIPAddressField(unique=True, protocol='IPv4', verbose_name='Endereço IP')
 
     def get_absolute_url(self):
         from django.urls import reverse
@@ -26,4 +35,20 @@ class IP(Base):
     class Meta:
         verbose_name = 'Endereço IP'
         verbose_name_plural = 'Endereços IPs'
+        ordering = ['register_in']
+
+
+class ElectronicEquipment(Base):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, max_length=80,
+                                   verbose_name='Departamento')
+    equipment = models.CharField(max_length=80, verbose_name='Identificação')
+    type = models.CharField(max_length=8, choices=EQUIPMENT_TYPE_CHOICES, verbose_name='Tipo')
+    ip = models.OneToOneField(IP, on_delete=models.CASCADE, verbose_name='IP')
+
+    def __str__(self):
+        return self.equipment
+
+    class Meta:
+        verbose_name = 'Equipamento'
+        verbose_name_plural = 'Equipamentos'
         ordering = ['register_in']
