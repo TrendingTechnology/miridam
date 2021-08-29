8ce6bcf6833f0fb8db1308b17c83084f881e56e9
ssousuke <wesleysousukee@gmail.com>
2021-08-29 13:23:07 -0300

    classe de listar departamentos

--- department/views.py
+++ department/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from department.models import Department
 
 
-# Create your views here.
-def department(request):
-    pass
+class DepartmentListView(generic.ListView):
+    model = Department
+    template_name = 'department_list.html'
+    context_object_name = 'departments'
