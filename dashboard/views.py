61b2b4584be5d67be55f1f0eb8902d6ce84fb97b
wesleysousuke <wesleysousukee@Gmail.com>
2021-09-10 01:53:30 -0300

    dashboard: contagem dos itens

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
+from equipments.models import ElectronicEquipment, IP
+from department.models import Department
 
 
 class DashboardView(generic.View):
-    template_name = 'index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        template_name = 'index.html'
+        count_equipments = ElectronicEquipment.objects.all().count()
+        count_ip = IP.objects.count()
+        count_departments = Department.objects.all().count()
+        context = {
+            'count_equipments': count_equipments,
+            'count_ip': count_ip,
+            'count_departments': count_departments,
+        }
+        return render(request, template_name, context)
