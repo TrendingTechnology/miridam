5eef3b2c761c2b554b257cc9d60df4e2c15bfbfa
wesleysousuke <wesleysousukee@Gmail.com>
2021-09-13 11:43:40 -0300

    adiciona novas funções ao admin

--- department/admin.py
+++ department/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from department.models import Department
 
-admin.site.register(Department)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'department', 'supervisor', 'register_in', 'updated_in', 'activated',)
+    list_display_links = ('id', 'department', 'supervisor', 'register_in', 'updated_in',)
+    list_editable = ('activated',)
