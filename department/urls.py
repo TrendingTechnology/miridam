f99c0179b1e3e90ce0914e248fe32ab87d8d105a
wesleysousuke <wesleysousukee@Gmail.com>
2021-09-14 11:19:35 -0300

    adiciona url de update

--- department/urls.py
+++ department/urls.py
@@ -5,5 +5,6 @@ app_name = 'departments'
 urlpatterns = [
     path('lista/', views.DepartmentListView.as_view(), name='department_list'),
     path('adicionar/', views.DepartmentCreateView.as_view(), name='department_create'),
+    path('editar/<int:pk>', views.DepartmentEditView.as_view(), name='department_edit'),
 
 ]
