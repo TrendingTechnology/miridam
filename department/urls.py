c99248e181d93c492b298eafca35139d9c1edba7
wesleysousuke <wesleysousukee@Gmail.com>
2021-09-14 17:36:13 -0300

    adiciona view de detalhes e delete

--- department/urls.py
+++ department/urls.py
@@ -4,7 +4,12 @@ from department import views
 app_name = 'departments'
 urlpatterns = [
     path('lista/', views.DepartmentListView.as_view(), name='department_list'),
-    path('adicionar/', views.DepartmentCreateView.as_view(), name='department_create'),
-    path('editar/<int:pk>', views.DepartmentEditView.as_view(), name='department_edit'),
-
+    path('adicionar/', views.DepartmentCreateView.as_view(),
+         name='department_create'),
+    path('detalhes/<int:pk>', views.DepartmentEditView.as_view(),
+         name='department_edit'),
+    path('editar/<int:pk>', views.DepartmentDetailView.as_view(),
+         name='department_detail'),
+    path('deletar/<int:pk>', views.DepartmentDeleteView.as_view(),
+         name='department_delete')
 ]
